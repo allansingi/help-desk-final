43255faf1e54d5e66ca834847701f3958d8bf6f1
Allan Borges <91190584+allansingi@users.noreply.github.com>
2024-09-18 16:25:00 +0100

    feat: add validation email already exists

--- user-service-api/src/main/java/pt/allanborges/userserviceapi/service/UserService.java
+++ user-service-api/src/main/java/pt/allanborges/userserviceapi/service/UserService.java
@@ -2,9 +2,9 @@ package pt.allanborges.userserviceapi.service;
 
 import lombok.RequiredArgsConstructor;
 import models.exceptions.ResourceNotFoundException;
-
 import models.requests.CreateUserRequest;
 import models.responses.UserResponse;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import pt.allanborges.userserviceapi.mapper.UserMapper;
 import pt.allanborges.userserviceapi.repository.UserRepository;
@@ -24,7 +24,16 @@ public class UserService {
     }
 
     public void save(CreateUserRequest createUserRequest) {
+        verifyIfEmailAlreadyExists(createUserRequest.email(), null);
         userRepository.save(userMapper.fromRequest(createUserRequest));
+    }
+
+    private void verifyIfEmailAlreadyExists(final String email, final String id) {
+        userRepository.findByEmail(email)
+                .filter(user -> !user.getId().equals(id))
+                .ifPresent(user -> {
+                    throw new DataIntegrityViolationException("Email [ " + email + " ] already exists");
+                });
     }
 
 }
