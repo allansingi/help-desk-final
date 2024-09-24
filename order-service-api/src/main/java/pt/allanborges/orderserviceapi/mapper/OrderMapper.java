d8facb5012b6d4ce30f30741eb85d046af9a6c5c
Allan Borges <91190584+allansingi@users.noreply.github.com>
2024-09-24 11:41:14 +0100

    fix: bug fix on create new order

--- order-service-api/src/main/java/pt/allanborges/orderserviceapi/mapper/OrderMapper.java
+++ order-service-api/src/main/java/pt/allanborges/orderserviceapi/mapper/OrderMapper.java
@@ -7,6 +7,9 @@ import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import pt.allanborges.orderserviceapi.entities.Order;
 
+import java.time.LocalDateTime;
+
+import static java.time.LocalDateTime.now;
 import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
 import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
 
@@ -19,11 +22,16 @@ public interface OrderMapper {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "status", source = "status", qualifiedByName = "mapStatus")
+    @Mapping(target = "createdAt", expression = "java(mapCreatedAt())")
     Order fromRequest(CreateOrderRequest request);
 
     @Named("mapStatus")
     default OrderStatusEnum mapStatus(final String status) {
         return OrderStatusEnum.toEnum(status);
+    }
+
+    default LocalDateTime mapCreatedAt() {
+        return now();
     }
 
 }
